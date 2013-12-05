cf8cb59690d7f89f4b42e8d424baf6c053c8a494
kimyee <kimyee.16@gmail.com>
2013-12-05 13:10:13 -0500

    Rounding item prices to 2 decimal points

--- database.rb
+++ database.rb
@@ -14,19 +14,19 @@ class Database
 			@input_quantity = "#{item.item_quantity}"
 			@input_name = "#{item.item_name}"	
 			@input_price = "#{item.item_price}"
-			@price = @input_price.to_i
+			@price = @input_price.to_f.round(2)
 
 		if @input_name.include?("imported") && @input_name.include?("music") || @input_name.include?("perfume")
 			@import_taxed = (@price * 0.15) + @price
-			puts "#{@input_quantity } #{@input_name}: #{@import_taxed}"
+			puts "#{@input_quantity } #{@input_name}: #{@import_taxed.round(2)}"
 		elsif @input_name.include?("imported") 
 			@import_price = (@price * 0.05) + @price
-			puts "#{@input_quantity } #{@input_name}: #{@import_price}"
+			puts "#{@input_quantity } #{@input_name}: #{@import_price.round(2)}"
 		elsif @input_name.include?("music") || @input_name.include?("perfume")
 			@taxed_price = (@price * 0.10) + @price
-			puts "#{@input_quantity} #{@input_name}: #{@taxed_price}"
+			puts "#{@input_quantity} #{@input_name}: #{@taxed_price.round(2)}"
 		else
-			puts "#{@input_quantity} #{@input_name}: #{@price}"
+			puts "#{@input_quantity} #{@input_name}: #{@price.round(2)}"
 		end
 
 		# @price if @price != nil
@@ -44,7 +44,12 @@ class Database
 		# 	print @import_taxed + @import_price
 		# end
 
+		# if @price != nil && @taxed_price != nil
+		# 	puts "Total: "
+		# 	puts @price + @taxed_price
 		# end
+
+		end
 
 
 

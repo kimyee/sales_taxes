0ea2010ab395877574b42613ef48327f5e627eb4
kimyee <kimyee.16@gmail.com>
2013-12-05 12:54:07 -0500

    Edit adding 15% tax to items

--- database.rb
+++ database.rb
@@ -1,12 +1,6 @@
 class Database
 	@items = []
 	
-	@exceptions = ["chocolate bar","book","pills"]
-	@tax_free = @exceptions.to_s
-	
-	@to_tax = ["music","perfume"]
-	@taxable = @to_tax.to_s
-	
 	def self.basket
 		@basket
 	end
@@ -22,18 +16,35 @@ class Database
 			@input_price = "#{item.item_price}"
 			@price = @input_price.to_i
 
-		if @input_name.include?("imported") 
+		if @input_name.include?("imported") && @input_name.include?("music") || @input_name.include?("perfume")
+			@import_taxed = (@price * 0.15) + @price
+			puts "#{@input_quantity } #{@input_name}: #{@import_taxed}"
+		elsif @input_name.include?("imported") 
 			@import_price = (@price * 0.05) + @price
 			puts "#{@input_quantity } #{@input_name}: #{@import_price}"
-		elsif @input_name.include?("m")
+		elsif @input_name.include?("music") || @input_name.include?("perfume")
 			@taxed_price = (@price * 0.10) + @price
 			puts "#{@input_quantity} #{@input_name}: #{@taxed_price}"
 		else
 			puts "#{@input_quantity} #{@input_name}: #{@price}"
 		end
 
+		# @price if @price != nil
+		# @taxed_price if @taxed_price != nil
+		# @import_price if @import_price != nil
 
-		end
+		# if @price != nil && @taxed_price != nil && @import_price != nil && @import_taxed != nil
+		# 	print "Total: "
+		# 	print @price + @taxed_price + @import_price + @import_taxed
+		# elsif @price !=nil && @taxed_price != nil
+		# 	print "Total: "
+		# 	print @price + @taxed_price
+		# elsif @import_taxed != nil && @taxed_price != nil
+		# 	print "Total: "	
+		# 	print @import_taxed + @import_price
+		# end
+
+		# end
 
 
 
